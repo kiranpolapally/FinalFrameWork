639dea3d4d79ffbdcfb2759b2fc578eb19a8c301
kiranpolapally <kiranpolapally15@gmail.com>
2018-08-05 21:57:33 +0530

    Add files via upload

--- Excelweb.java
+++ Excelweb.java
@@ -1,11 +1,5 @@
 package SGB;
 
-import java.awt.AWTException;
-import java.awt.Robot;
-import java.awt.Toolkit;
-import java.awt.datatransfer.Clipboard;
-import java.awt.datatransfer.StringSelection;
-import java.awt.event.KeyEvent;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
@@ -14,8 +8,6 @@ import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbookType;
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -25,7 +17,6 @@ import org.testng.annotations.Test;
 
 
 public class Excelweb {
-	
 	
 	@Test
 	public void f() throws Exception{
@@ -41,13 +32,20 @@ public class Excelweb {
 		XSSFWorkbook wb = new XSSFWorkbook(fi);  //xlsx format
 		XSSFSheet sh = wb.getSheet(SheetName); // sheet name
 		int rowCount = sh.getLastRowNum()+1;
+		System.out.println(rowCount+"123456");
 		for (int i = 1; i <rowCount; i++) {
 		XSSFRow r = sh.createRow(i);     // Refernece here 0 indicate the row number
 		String k =ExcelRead(i,0);
-		String value = driver.findElement(By.xpath("//table[@id='customers']//td[text()='"+k+"']/../td[3]")).getText();
-		Cell c = r.createCell(i);  // here 0 indicate the column. COlumn always start with O
-		c.setCellValue(value);
+		
+		if (!k.equals(null) || k.equals("")) {
+			String value = driver.findElement(By.xpath("//table[@id='customers']//td[text()='"+k+"']/../td[3]")).getText();
+			Cell c = r.createCell(3);  // here 0 indicate the column. COlumn always start with O
+			c.setCellValue(value);
 		}
+		else {
+			System.out.println("invalid data in cell");
+		}
+	}
 		FileOutputStream fo = new FileOutputStream(FilePath);
 		wb.write(fo);
 		fo.close();
@@ -66,14 +64,14 @@ public class Excelweb {
 		int rowcount = sh.getRow(0).getLastCellNum();
 		XSSFRow r = sh.getRow(row);  //row
 		String cellvalue = d.formatCellValue(r.getCell(Column));  // column
-		System.out.println("cellvalue"+cellvalue);
+//		System.out.println("cellvalue"+cellvalue);
 		wb.close();
 		return cellvalue;
 		
 	}
 
 
-	public String Excelwrite(int row, int Column, String data) throws Exception{
+	    public String Excelwrite(int row, int Column, String data) throws Exception{
 		DataFormatter d = new DataFormatter();
 		String FilePath = "E:\\kishore.xlsx";
 		String SheetName = "Sheet1";
@@ -81,12 +79,12 @@ public class Excelweb {
 		FileInputStream fi = new FileInputStream(FilePath);  //Path
 		XSSFWorkbook wb = new XSSFWorkbook(fi);  //xlsx format
 		XSSFSheet sh = wb.getSheet(SheetName); // sheet name
-		int rowcount = sh.getRow(0).getLastCellNum();
+		int rowcount = sh.getRow(1).getLastCellNum();
 		XSSFRow r = sh.getRow(row);  //row
 		String cellvalue = d.formatCellValue(r.getCell(Column));  // column
 		System.out.println("cellvalue"+cellvalue);
 		FileOutputStream fo = new FileOutputStream(FilePath);
-		XSSFRow ro = sh.createRow(row);     // Refernece here 0 indicate the row number
+		XSSFRow ro = sh.createRow(row);     // Reference here 0 indicate the row number
 		Cell c = ro.createCell(Column);  // here 0 indicate the column. COlumn always start with O
 		c.setCellValue(data);
 		wb.write(fo);
